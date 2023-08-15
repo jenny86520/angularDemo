66a6bf6dce584da7d97444a2cb0ed590632038a6
unknown <00596153@cathaybk.intra.uwccb>
2023-08-15 10:37:59 +0800

    [Angular 基礎課程] 5_Directive
    Changes to be committed:
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	new file:   src/app/components/demo2/demo2.component.css
    	new file:   src/app/components/demo2/demo2.component.html
    	new file:   src/app/components/demo2/demo2.component.spec.ts
    	new file:   src/app/components/demo2/demo2.component.ts
    	new file:   src/app/directives/highlight.directive.spec.ts
    	new file:   src/app/directives/highlight.directive.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Demo1Component } from './components/demo1/demo1.component';
 import { FormsModule } from '@angular/forms';
+import { Demo2Component } from './components/demo2/demo2.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Demo1Component
+    Demo1Component,
+    Demo2Component,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/demo2/demo2.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  templateUrl: './demo2.component.html',
+  styleUrls: ['./demo2.component.css']
+})
+export class Demo2Component implements OnInit {
+  count: number = 0;
+  isRed: boolean = false;
+  ngIfFlag: boolean = false;
+  array: number[] = [1, 2, 3, 4, 5];
+  number: string = '0';
+  highlightColor = 'yellow';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  add() {
+    this.count++;
+  }
+  sub() {
+    this.count--;
+  }
+
+  changeClass() {
+    this.isRed = !this.isRed;
+  }
+
+  changeNgIfFlag() {
+    this.ngIfFlag = !this.ngIfFlag;
+  }
+
+  push() {
+    this.array.push(+this.number);
+  }
+
+  remove() {
+    this.array = this.array.filter(x => x !== +this.number);
+  }
+
+  changeHighlightColor() {
+    this.highlightColor = this.highlightColor === 'yellow' ? 'red' : 'yellow';
+  }
+}
