7964fb2bced9d273fe4709c2ff87e610c8b9966f
Aka <NT96153@cathaybk.com.tw>
2023-08-25 17:28:24 +0800

    [feat] Add encapsulation settings
    Changes to be committed:
    	modified:   src/app/components/demo1/demo1.component.ts

--- src/app/components/demo1/demo1.component.ts
+++ src/app/components/demo1/demo1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-demo1',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
   //     h3 {
   //       color: dark;
   //     }`]
+  // encapsulation: ViewEncapsulation.Emulated,
 })
 export class Demo1Component implements OnInit {
   name: string = 'Aka';
