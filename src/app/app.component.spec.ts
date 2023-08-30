33aab9251620b66b5b0fc03f0db56e74396150f0
Aka <NT96153@cathaybk.com.tw>
2023-08-30 16:13:24 +0800

    [feat] UnitTest: Add Test Lifecycle
    Changes to be committed:
    	modified:   src/app/app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,37 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+describe('Test Lifecycle', () => {
+  beforeAll(() => {
+    console.log('Run beforeAll');
+  });
+
+  beforeEach(() => {
+    console.log('Run beforeEach');
+  });
+
+  it('Test case 1', () => {
+    expect('Run it 1').toBeDefined();
+  });
+  it('Test case 2', () => {
+    expect('Run it 2').toBeDefined();
+  });
+
+  afterEach(() => {
+    console.log('Run afterEach');
+  });
+
+  afterAll(() => {
+    console.log('Run afterAll');
+  });
+});
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [],
     }).compileComponents();
   });
 
@@ -30,6 +52,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularDemo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angularDemo app is running!'
+    );
   });
 });
