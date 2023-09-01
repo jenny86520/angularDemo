badeedb67fdc98b4e5744f346549d02c2b55eaa5
Aka <NT96153@cathaybk.com.tw>
2023-09-01 16:33:54 +0800

    [feat] UnitTest: Add DI Service
    Changes to be committed:
    	modified:   src/app/services/validator.service.spec.ts

--- src/app/services/validator.service.spec.ts
+++ src/app/services/validator.service.spec.ts
@@ -1,16 +1,89 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { PasswordValidator } from './validator.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { ApiService } from './api.service';
 
 describe('PasswordValidator', () => {
   let service: PasswordValidator;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, ReactiveFormsModule],
+    });
     service = TestBed.inject(PasswordValidator);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // 練習 SPY DI
+  it('#validate should return stubbed value from a spy', (done: DoneFn) => {
+    // 產生一個 ApiService.getValidationResult 實作一個回傳值供注入測試
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', [
+      'getValidationResult',
+    ]);
+    apiServiceSpy.getValidationResult.and.returnValue(of(true));
+    // DI 假 ApiService
+    service = new PasswordValidator(apiServiceSpy);
+
+    service.validate(new FormControl('1111')).subscribe((result) => {
+      expect(result).withContext('return value').toBeNull();
+      done();
+    });
+
+    apiServiceSpy.getValidationResult.calls
+      .mostRecent()
+      .returnValue.subscribe((result: boolean) => {
+        expect(result)
+          .withContext('spy method latest called return value')
+          .toBeTrue();
+        done();
+      });
+
+    expect(apiServiceSpy.getValidationResult.calls.count())
+      .withContext('spy method was called once')
+      .toBe(1);
+  });
+});
+
+// 練習 SPY DI - use TestBed
+describe('PasswordValidator SPY DI Test Use TestBed', () => {
+  let service: PasswordValidator;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('ApiService', ['getValidationResult']);
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, ReactiveFormsModule],
+      providers: [PasswordValidator, { provide: ApiService, useValue: spy }],
+    });
+    service = TestBed.inject(PasswordValidator);
+    apiServiceSpy = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+  });
+
+  it('#validate should return stubbed value from a spy', (done: DoneFn) => {
+    apiServiceSpy.getValidationResult.and.returnValue(of(true));
+
+    service.validate(new FormControl('1111')).subscribe((result) => {
+      expect(result).withContext('return value').toBeNull();
+      done();
+    });
+
+    apiServiceSpy.getValidationResult.calls
+      .mostRecent()
+      .returnValue.subscribe((result: boolean) => {
+        expect(result)
+          .withContext('spy method latest called return value')
+          .toBeTrue();
+        done();
+      });
+
+    expect(apiServiceSpy.getValidationResult.calls.count())
+      .withContext('spy method was called once')
+      .toBe(1);
   });
 });
