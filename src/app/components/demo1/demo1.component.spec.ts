a4f7404a444ec89d6bad282f6d646d120254db2e
Aka <NT96153@cathaybk.com.tw>
2023-09-07 10:40:03 +0800

    [feat] UnitTest: Add jasmine.clock() test
    Changes to be committed:
    	modified:   src/app/components/demo1/demo1.component.spec.ts

--- src/app/components/demo1/demo1.component.spec.ts
+++ src/app/components/demo1/demo1.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Demo1Component } from './demo1.component';
+import { FormsModule } from '@angular/forms';
+
+// for jasmine.clock() if it not working
+// (window as any)['__zone_symbol__fakeAsyncPatchLock'] = true;
+// import 'zone.js/testing';
 
 describe('Demo1Component', () => {
   let component: Demo1Component;
@@ -8,9 +12,8 @@ describe('Demo1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ Demo1Component ]
-    })
-    .compileComponents();
+      declarations: [Demo1Component],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +24,31 @@ describe('Demo1Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+
+describe('Change Text', () => {
+  let component: Demo1Component;
+  let fixture: ComponentFixture<Demo1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [Demo1Component],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Demo1Component);
+    component = fixture.componentInstance;
+    jasmine.clock().install();
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
+
+  it('text be changed', () => {
+    jasmine.clock().tick(1000);
+    expect(component.name).toBe('Test Success!');
   });
 });
