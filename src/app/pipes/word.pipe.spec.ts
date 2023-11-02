0f33b48608fa38b45fd728dc73b6bfa2fce4e55c
Aka <NT96153@cathaybk.com.tw>
2023-11-02 16:21:16 +0800

    [feat] Unit Test: Add pipe test
    Changes to be committed:
    	modified:   src/app/pipes/word.pipe.spec.ts

--- src/app/pipes/word.pipe.spec.ts
+++ src/app/pipes/word.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { WordPipe } from './word.pipe';
 
-describe('WordPipe', () => {
-  it('create an instance', () => {
-    const pipe = new WordPipe();
-    expect(pipe).toBeTruthy();
+fdescribe('WordPipe', () => {
+  let pipe: WordPipe;
+  beforeEach(() => {
+    pipe = new WordPipe();
+  });
+
+  it('can transform "abcdefg" to "abc..." by default', () => {
+    expect(pipe.transform('abcdefg')).toBeTruthy();
+  });
+
+  it('can transform "abcdefg" to "abc==="', () => {
+    expect(pipe.transform('abcdefg', '===')).toBeTruthy();
   });
 });
